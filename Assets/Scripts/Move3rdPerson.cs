13d8298194c7c37a343b370b8db46e8dec4a4cfe
Julian.B <julian.b@eternal-dreams.fr>
2023-05-12 00:30:47 +0200

    Scene with Spawn (Base SpaceShip and Character with 3D camera view)

---
+++ Assets/Scripts/Move3rdPerson.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+
+public class Move3rdPerson : MonoBehaviour
+{
+    public Transform player; // Référence à l'objet joueur
+    public float smoothSpeed = 0.125f; // Vitesse de mouvement de la caméra
+
+    public Vector3 offset; // Distance entre la caméra et le joueur
+
+    public float rotateSpeed = 5f; // Vitesse de rotation de la caméra
+
+    float mouseX, mouseY; // Stocker les mouvements de la souris
+
+    void Start()
+    {
+        Cursor.visible = false;
+    }
+
+    void FixedUpdate()
+    {
+        // Mouvements de la souris pour faire pivoter la caméra
+        mouseX += Input.GetAxis("Mouse X") * rotateSpeed;
+        mouseY -= Input.GetAxis("Mouse Y") * rotateSpeed;
+        mouseY = Mathf.Clamp(mouseY, -35f, 60f);
+
+        // Calculer la rotation de la caméra en fonction des mouvements de la souris
+        Quaternion rotation = Quaternion.Euler(mouseY, mouseX, 0);
+
+        // Calculer la position désirée de la caméra en fonction de la rotation et de la position du joueur
+        Vector3 desiredPosition = player.position + (rotation * offset);
+
+        // Vérifier si la caméra entre en collision avec un objet et ajuster la position en conséquence
+        RaycastHit hit;
+        if (Physics.Linecast(player.position, desiredPosition, out hit))
+        {
+            // Si la collision est détectée, rapprocher la caméra de l'objet pour éviter la collision
+            desiredPosition = hit.point + (hit.normal * 0.2f);
+        }
+
+        // Déplacer la caméra en douceur vers la position et la rotation désirées
+        transform.position = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+        transform.rotation = Quaternion.Lerp(transform.rotation, rotation, smoothSpeed);
+    }
+}
